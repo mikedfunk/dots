e332875392278dc55474c647567e7a37926a50d6
Mike Funk <mike@mikefunk.com>
2023-01-17 12:06:46 -0800

    working

--- .support/cscope-find.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-# ag --skip-vcs-ignores --files-with-matches -g ".*.php$" `cat ~/.ctags.d/config.ctags | grep --regexp "^--exclude" | sed s/--exclude/--ignore/`
-# This is so I can ignore _ide_helper.php in searches but include it in ctags and cscope db.
-ag --unrestricted --files-with-matches -g ".*.php$" `cat ~/.ctags.d/config.ctags | grep --regexp "^--exclude" | sed s/--exclude/--ignore/`
