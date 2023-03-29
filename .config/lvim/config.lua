e342ed409ab6aa1b6e5be6d950cb3e8eb79e08fe
Mike Funk <mike@mikefunk.com>
2023-03-29 11:29:31 -0700

    working

--- .config/lvim/config.lua
+++ .config/lvim/config.lua
@@ -1209,6 +1209,14 @@ lvim.builtin.theme.tokyonight.options.day_brightness = 0.15 -- high contrast but
 lvim.builtin.theme.tokyonight.options.lualine_bold = true -- section headers in lualine theme will be bold
 lvim.builtin.theme.tokyonight.options.hide_inactive_statusline = true
 -- vim.cmd 'silent! hi! link TabLineFill BufferLineGroupSeparator' -- temp workaround to bufferline background issue
+
+lvim.builtin.theme.tokyonight.options.on_highlights = function(hl, c)
+  local util = require 'tokyonight.util'
+  -- change the fold color to be more subtle in both night and day modes
+  hl.Folded = {
+    bg = util.darken(c.fg_gutter, 0.1), -- yes, the bg was set to an fg color in tokyonight
+  }
+end
 -- }}}
 
 -- which-key.nvim {{{
