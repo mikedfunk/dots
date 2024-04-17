4da55448b212450855bfd901cf26c17a969d6832
Mike Funk <mike@mikefunk.com>
2024-04-17 01:40:04 -0700

    working

--- .config/nvim/lua/plugins/group_luasnip.lua
+++ .config/nvim/lua/plugins/group_luasnip.lua
@@ -6,6 +6,7 @@ return {
       config = function()
         require("luasnip.loaders.from_vscode").lazy_load()
         require("luasnip.loaders.from_snipmate").lazy_load()
+        require("luasnip.loaders.from_lua").lazy_load()
       end,
     },
   },
