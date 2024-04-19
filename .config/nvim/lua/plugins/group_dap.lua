850ad5a2bde188e9105f0be28c442d16f9af30ea
Mike Funk <mike@mikefunk.com>
2024-04-19 14:40:09 -0700

    working

--- .config/nvim/lua/plugins/group_dap.lua
+++ .config/nvim/lua/plugins/group_dap.lua
@@ -1,9 +1,88 @@
 return {
-  {
-    "jay-babu/mason-nvim-dap.nvim",
-    opts = {
-      ensure_installed = {
-        "php",
+  "mfussenegger/nvim-dap",
+  dependencies = {
+    {
+      "LiadOz/nvim-dap-repl-highlights",
+      dependencies = {
+        "mfussenegger/nvim-dap",
+        {
+          "nvim-treesitter/nvim-treesitter",
+          opts = {
+            ensure_installed = { "dap_repl" },
+          },
+        },
+      },
+      config = true,
+    },
+    {
+      "jay-babu/mason-nvim-dap.nvim",
+      opts = {
+        ensure_installed = { "php" },
+      },
+    },
+    {
+      "nvim-telescope/telescope-dap.nvim",
+      dependencies = { "mfussenegger/nvim-dap", "nvim-telescope/telescope.nvim" },
+      keys = {
+        { "<Leader>dL", "<Cmd>Telescope dap breakpoints<CR>", noremap = true, desc = "Telescope List Breakpoints" },
+        { "<Leader>dV", "<Cmd>Telescope dap variables<CR>", noremap = true, desc = "Telescope List Variables" },
+        { "<Leader>dF", "<Cmd>Telescope dap frames<CR>", noremap = true, desc = "Telescope Call Stack" },
+      },
+      config = function()
+        require("telescope").load_extension("dap")
+      end,
+    },
+    {
+      "ofirgall/goto-breakpoints.nvim",
+      dependencies = { "mfussenegger/nvim-dap" },
+      keys = {
+        {
+          "]B",
+          function()
+            require("goto-breakpoints").next()
+          end,
+          desc = "Go to Next Breakpoint",
+        },
+        {
+          "[B",
+          function()
+            require("goto-breakpoints").next()
+          end,
+          desc = "Go to Previous Breakpoint",
+        },
+      },
+    },
+    {
+      "Weissle/persistent-breakpoints.nvim",
+      dependencies = { "mfussenegger/nvim-dap" },
+      opts = {
+        load_breakpoints_event = { "BufReadPost" },
+      },
+      keys = {
+        {
+          "<leader>dT",
+          function()
+            require("persistent-breakpoints.api").toggle_breakpoint()
+          end,
+          noremap = true,
+          desc = "Toggle Breakpoint (Persistent)",
+        },
+        {
+          "<Leader>dX",
+          function()
+            require("persistent-breakpoints.api").clear_all_breakpoints()
+          end,
+          noremap = true,
+          desc = "Clear All Breakpoints (Persistent)",
+        },
+        {
+          "<Leader>dE",
+          function()
+            require("persistent-breakpoints.api").set_conditional_breakpoint()
+          end,
+          noremap = true,
+          desc = "Breakpoint Condition (Persistent)",
+        },
       },
     },
   },
--- .config/nvim/lua/plugins/group_telescope.lua
+++ .config/nvim/lua/plugins/group_telescope.lua
@@ -1,32 +1,34 @@
 return {
-  "nvim-telescope/telescope.nvim",
-  dependencies = {
-    {
-      "debugloop/telescope-undo.nvim",
-      keys = {
-        { "<Leader>U", "<Cmd>Telescope undo<CR>", desc = "Telescope Undo" },
+  {
+    "nvim-telescope/telescope.nvim",
+    dependencies = {
+      {
+        "debugloop/telescope-undo.nvim",
+        keys = {
+          { "<Leader>U", "<Cmd>Telescope undo<CR>", desc = "Telescope Undo" },
+        },
       },
     },
-  },
-  opts = function(_, opts)
-    opts.extensions = vim.tbl_deep_extend("force", opts.extensions or {}, {
-      undo = {
-        mappings = {
-          i = {
-            ["<cr>"] = require("telescope-undo.actions").restore,
+    opts = function(_, opts)
+      opts.extensions = vim.tbl_deep_extend("force", opts.extensions or {}, {
+        undo = {
+          mappings = {
+            i = {
+              ["<cr>"] = require("telescope-undo.actions").restore,
+            },
           },
         },
-      },
-    })
+      })
 
-    opts.defaults = vim.tbl_deep_extend("force", opts.defaults or {}, {
-      mappings = {
-        i = {
-          ["<Esc>"] = "close",
+      opts.defaults = vim.tbl_deep_extend("force", opts.defaults or {}, {
+        mappings = {
+          i = {
+            ["<Esc>"] = "close",
+          },
         },
-      },
-    })
+      })
 
-    require("telescope").load_extension("undo")
-  end,
+      require("telescope").load_extension("undo")
+    end,
+  },
 }
