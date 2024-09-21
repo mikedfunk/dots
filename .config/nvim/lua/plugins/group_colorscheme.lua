ae6044cf2893010470f23636d2e18740fcddb05f
Mike Funk <mike@mikefunk.com>
2024-09-21 15:56:37 -0700

    working

--- .config/nvim/lua/plugins/group_colorscheme.lua
+++ .config/nvim/lua/plugins/group_colorscheme.lua
@@ -13,7 +13,7 @@ return {
           -- jack up all saturation, default is too dull!
           on_colors = function(colors)
             local hsluv = require("tokyonight.hsluv")
-            local multiplier = 1.1 -- I would like to go higher but I get "invalid highlight color"
+            local multiplier = 2.0
 
             for k, v in pairs(colors) do
               if type(v) == "string" and v ~= "NONE" then
@@ -24,17 +24,17 @@ return {
                 if vim.islist(v) then
                   for kk, vv in ipairs(v) do
                     if type(vv) == "string" and vv ~= "NONE" then
-                      local hsv = hsluv.hex_to_rgb(vv)
+                      local hsv = hsluv.hex_to_hsluv(vv)
                       hsv[2] = hsv[2] * multiplier > 100 and 100 or hsv[2] * multiplier
-                      colors[k][kk] = hsluv.rgb_to_hex(hsv)
+                      colors[k][kk] = hsluv.hsluv_to_hex(hsv)
                     end
                   end
                 else
                   for kk, vv in pairs(v) do
                     if type(vv) == "string" and vv ~= "NONE" then
-                      local hsv = hsluv.hex_to_rgb(vv)
+                      local hsv = hsluv.hex_to_hsluv(vv)
                       hsv[2] = hsv[2] * multiplier > 100 and 100 or hsv[2] * multiplier
-                      colors[k][kk] = hsluv.rgb_to_hex(hsv)
+                      colors[k][kk] = hsluv.hsluv_to_hex(hsv)
                     end
                   end
                 end
