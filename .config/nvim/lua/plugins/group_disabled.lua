f541534423062ba34d63bbd46faeb3d09264fc1f
Mike Funk <mike@mikefunk.com>
2024-10-28 16:14:45 -0700

    working

--- .config/nvim/lua/plugins/group_disabled.lua
+++ .config/nvim/lua/plugins/group_disabled.lua
@@ -3,6 +3,5 @@ return {
   -- { "nvimdev/dashboard-nvim", enabled = false },
   { "folke/flash.nvim", enabled = false },
   { "nvim-pack/nvim-spectre", enabled = false },
-  { "iamcco/markdown-preview.nvim", enabled = false },
   -- { "nvim-telescope/telescope-fzf-native.nvim", enabled = false },
 }
--- .config/nvim/lua/plugins/group_markdown.lua
+++ .config/nvim/lua/plugins/group_markdown.lua
@@ -1,16 +1,3 @@
 return {
-  {
-    "wallpants/github-preview.nvim",
-    dependencies = {
-      "folke/which-key.nvim",
-      opts = { spec = { { "<leader>m", group = "+markdown" } } },
-    },
-    ft = "markdown",
-    cmd = { "GithubPreviewStart", "GithubPreviewToggle", "GithubPreviewStop" },
-    keys = {
-      { "<Leader>mp", "<Cmd>GithubPreviewToggle<CR>", buffer = true, noremap = true, desc = "Preview Markdown" },
-    },
-    opts = {},
-  },
   { "bullets-vim/bullets.vim", ft = "markdown" },
 }
