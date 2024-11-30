cf22ad092b510bc7afb26b568533038c09285824
Mike Funk <mike@mikefunk.com>
2024-11-30 13:16:17 -0800

    working

--- .config/nvim/lua/plugins/group_luasnip.lua
+++ .config/nvim/lua/plugins/group_luasnip.lua
@@ -1,13 +1,35 @@
 return {
   "L3MON4D3/LuaSnip",
-  dependencies = {
+  keys = {
     {
-      "rafamadriz/friendly-snippets",
-      config = function()
-        require("luasnip.loaders.from_vscode").lazy_load({ paths = vim.env.HOME .. "/.config/nvim/snippets" })
-        require("luasnip.loaders.from_snipmate").lazy_load()
-        require("luasnip.loaders.from_lua").lazy_load()
+      "<a-l>",
+      function()
+        if require("luasnip").expand_or_jumpable() then
+          require("luasnip").expand_or_jump()
+        end
       end,
+      desc = "Expand snippet or jump forward",
+      mode = { "i", "s" },
+    },
+    {
+      "<a-h>",
+      function()
+        if require("luasnip").jumpable(-1) then
+          require("luasnip").jump(-1)
+        end
+      end,
+      desc = "Jump backward",
+      mode = { "i", "s" },
+    },
+    {
+      "<a-e>",
+      function()
+        if require("luasnip").choice_active() then
+          require("luasnip").change_choice(1)
+        end
+      end,
+      desc = "Next choice",
+      mode = { "i", "s" },
     },
   },
 }
