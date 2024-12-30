2d84a39158705b84c51bfb9111a52e76eaca5ea6
Mike Funk <mike@mikefunk.com>
2024-12-30 10:02:27 -0500

    working

--- .support/tmux-docker-status.sh
+++ .support/tmux-docker-status.sh
@@ -4,8 +4,11 @@
 DOCKER="󰡨"
 # DOCKER=""
 
-timeout 2s docker ps --quiet 1> /dev/null
-[[ $? != 0 ]] && echo "#[fg=red]${DOCKER}#[fg=default]" && exit 0
-echo -n "#[fg=green,bold]${DOCKER}#[fg=default,nobold]"
+# timeout 2s docker ps --quiet 1> /dev/null
+# [[ $? != 0 ]] && echo "#[fg=red]${DOCKER}#[fg=default]" && exit 0
+# echo -n "#[fg=green,bold]${DOCKER}#[fg=default,nobold]"
+[ "$(docker desktop status --format=json | jq .Status)" = '"running"' ] &&
+    echo -n "#[fg=green,bold]${DOCKER}#[fg=default,nobold]" ||
+    echo "#[fg=red]${DOCKER}#[fg=default]"
 
 [ -f "$HOME"/.support/saatchi/tmux-docker-status.sh ] && "$HOME"/.support/saatchi/tmux-docker-status.sh
