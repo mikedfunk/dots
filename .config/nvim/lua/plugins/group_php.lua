193e1e4eb44c39e5951b5ad0e6776d029c1afb32
Mike Funk <mike@mikefunk.com>
2025-01-17 17:59:46 -0800

    working

--- .config/nvim/lua/plugins/group_php.lua
+++ .config/nvim/lua/plugins/group_php.lua
@@ -68,4 +68,36 @@ return {
       },
     },
   },
+  {
+    "gbprod/phpactor.nvim",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "neovim/nvim-lspconfig",
+      {
+        "williamboman/mason.nvim",
+        opts_extend = { "ensure_installed" },
+        opts = {
+          ensure_installed = { "phpactor" },
+        },
+      },
+    },
+    filetypes = { "php" },
+    keys = {
+      { "<Leader>rm", "<Cmd>PhpActor context_menu<CR>", buffer = true, noremap = true, desc = "PHP Refactor Menu" },
+    },
+    cmd = { "PhpActor" },
+    opts = {
+      install = {
+        bin = vim.fn.stdpath("data") .. "/mason/packages/phpactor/phpactor.phar",
+        php_bin = vim.fn.executable("asdf") == 1
+            and table.concat(vim.fn.systemlist({ "asdf", "where", "php", "8.2.12" }), "") .. "/bin/php"
+          or "php",
+        path = vim.fn.stdpath("data") .. "/mason/packages/phpactor",
+        composer_bin = vim.fn.executable("asdf") == 1
+          and table.concat(vim.fn.systemlist({ "asdf", "where", "php", "8.2.12" }) or "composer", "")
+            .. "/.composer/vendor/bin/composer",
+      },
+      lspconfig = { enabled = false },
+    },
+  },
 }
