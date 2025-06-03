df302de3c4d32c90565066703b76146ac4cd4c9f
Mike Funk <mike@mikefunk.com>
2025-06-03 14:51:44 -0700

    working

--- .support/tmux-spotify-status.sh
+++ .support/tmux-spotify-status.sh
@@ -1,19 +1,5 @@
 #!/usr/bin/env bash
 
-# throttle, otherwise `spotify ...` will launch the spotify client when it's
-# quit because it's process hasn't finished exiting yet :/
-# sleep 4
-# if pgrep '^Spotify$' &>/dev/null; then
-#     if spotify status | head -n 1 | grep --silent playing; then
-#         status="󰐊"
-#     else
-#         status="󰏤"
-#     fi
-#     echo -n "$status $(spotify status artist): $(spotify status track)"
-# else
-#     echo -n "󰎊"
-# fi
-
 osascript <<'EOF'
 if application "Spotify" is running then
   tell application "Spotify"
