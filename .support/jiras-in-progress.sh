1f9d9b6e7e89cbc73deb04f33b4ad77a2feffcec
Mike Funk <mike@mikefunk.com>
2025-09-18 11:16:36 -0700

    working

--- .support/jiras-in-progress.sh
+++ .support/jiras-in-progress.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 icon=''
 keys=$(acli jira workitem search \
-    --jql 'assignee = currentUser() AND status = "In Progress" ORDER BY updated DESC' --json | jq -r 'map(.key) | join(", ")')
+    --jql 'assignee = currentUser() AND status = "In Progress" ORDER BY updated DESC' --json |
+    jq -r 'map(.key) | join(", ")')
 if [ -z "$keys" ]; then
     echo -n "$icon"
     exit 0
