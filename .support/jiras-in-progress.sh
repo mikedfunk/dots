ffe31ddc1b48b89d309df9229f8dc706e0c5f396
Mike Funk <mike@mikefunk.com>
2025-10-08 19:32:06 -0700

    working

--- .support/jiras-in-progress.sh
+++ .support/jiras-in-progress.sh
@@ -1,10 +1,47 @@
 #!/bin/bash
-icon=''
+# vim: set fdm=marker:
+
+# not cached version {{{
+
+# icon=''
+# keys=$(acli jira workitem search \
+#     --jql 'assignee = currentUser() AND status = "In Progress" ORDER BY updated DESC' --json |
+#     jq -r 'map(.key) | join(", ")')
+# if [ -z "$keys" ]; then
+#     echo -n "$icon"
+#     exit 0
+# fi
+# }}}
+
+# cached version (run every 5 mins): {{{
+
+CACHE_FILE="/tmp/tmux_jira_status.cache"
+ICON=''
+CACHE_TTL=$((5 * 60)) # 5 minutes
+
+# Check if cache exists and is fresh
+if [ -f "$CACHE_FILE" ]; then
+    LAST_MODIFIED=$(stat -f "%m" "$CACHE_FILE") # macOS
+    NOW=$(date +%s)
+    AGE=$((NOW - LAST_MODIFIED))
+
+    if [ "$AGE" -lt "$CACHE_TTL" ]; then
+        cat "$CACHE_FILE"
+        exit 0
+    fi
+fi
+
+# Generate new status
 keys=$(acli jira workitem search \
     --jql 'assignee = currentUser() AND status = "In Progress" ORDER BY updated DESC' --json |
     jq -r 'map(.key) | join(", ")')
+
 if [ -z "$keys" ]; then
-    echo -n "$icon"
+    printf "%s\n" "$ICON" >"$CACHE_FILE"
+    cat "$CACHE_FILE"
     exit 0
 fi
-echo -n "$icon $keys"
+
+printf "%s %s\n" "$ICON" "$keys" >"$CACHE_FILE"
+cat "$CACHE_FILE"
+# }}}
