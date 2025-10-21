5fe8daf1ccef33f8dcb60fb31ed00a1062a224c4
Mike Funk <mike@mikefunk.com>
2025-10-20 18:19:31 -0700

    working

--- install.sh
+++ install.sh
@@ -4,8 +4,8 @@ _log_info() { echo -e "$(tput setaf 2)$(tput rev)$(tput bold) \xE2\x9C\x93 $1 $(
 
 _install_yadm_if_missing() {
     _log_info "installing yadm"
-    curl -fLo /usr/local/bin/yadm https://github.com/yadm-dev/yadm/raw/master/yadm && chmod a+x /usr/local/bin/yadm
-    export PATH="/usr/local/bin:$PATH"
+    curl -fLo "$HOME/.bin/yadm" https://github.com/yadm-dev/yadm/raw/master/yadm && chmod +x "$HOME/.bin/yadm"
+    export PATH="$HOME/.bin:$PATH"
 }
 
 _install_yadm_if_missing
