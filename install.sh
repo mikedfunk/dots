476c51881e0767dfd21024e7b0559afbc31cf90a
Mike Funk <mike@mikefunk.com>
2025-10-20 22:41:08 -0700

    working

--- install.sh
+++ install.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
-sh -c "$(curl -fsLS raw.githubusercontent.com/yadm-dev/yadm/master/yadm)" -- clone git@github.com:mikedfunk/dots.git --bootstrap
+curl -fLo "$HOME"/.bin/yadm https://github.com/yadm-dev/yadm/raw/master/yadm >/tmp/yadm
+chmod +x /tmp/yadm
+
+/tmp/yadm clone git@github.com:mikedfunk/dots.git --bootstrap
