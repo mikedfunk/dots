ba77d5638d142ac4b19a8a9f6177a1d920690cac
Mike Funk <mike@mikefunk.com>
2025-10-20 19:23:35 -0700

    working

--- install.sh
+++ install.sh
@@ -2,12 +2,6 @@
 
 _log_info() { echo -e "$(tput setaf 2)$(tput rev)$(tput bold) \xE2\x9C\x93 $1 $(tput sgr0)"; }
 
-_install_yadm_if_missing() {
-    _log_info "installing yadm"
-    mkdir -p "$HOME"/.bin
-    curl -fLo "$HOME"/.bin/yadm https://github.com/yadm-dev/yadm/raw/master/yadm && chmod +x "$HOME"/.bin/yadm
-    export PATH="$HOME/.bin:$PATH"
-}
-
-_install_yadm_if_missing
+curl https://mise.run | sh
+mise use yadm -y -q
 bash -c "$HOME/.bin/yadm clone git@github.com:mikedfunk/dots.git --bootstrap"
