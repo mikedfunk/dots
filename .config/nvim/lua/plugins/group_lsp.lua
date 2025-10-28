cb0b51b81d443a1227eaa4ac6dc4003bd169b1ad
Mike Funk <mike@mikefunk.com>
2025-10-28 15:04:10 -0700

    working

--- .config/nvim/lua/plugins/group_lsp.lua
+++ .config/nvim/lua/plugins/group_lsp.lua
@@ -152,7 +152,26 @@ return {
     opts = {},
     dependencies = { "mason-org/mason.nvim" },
   },
-  { "antosha417/nvim-lsp-file-operations", event = "LspAttach", opts = {} },
+  -- {
+  --   "antosha417/nvim-lsp-file-operations",
+  --   dependencies = { "nvim-lua/plenary.nvim", "neovim/nvim-lspconfig" },
+  --   event = "LspAttach",
+  --   opts = {},
+  --   init = function()
+  --     local lspconfig = require("lspconfig")
+  --
+  --     -- Set global defaults for all servers
+  --     lspconfig.util.default_config = vim.tbl_extend("force", lspconfig.util.default_config, {
+  --       capabilities = vim.tbl_deep_extend(
+  --         "force",
+  --         vim.lsp.protocol.make_client_capabilities(),
+  --         -- returns configured operations if setup() was already called
+  --         -- or default operations if not
+  --         require("lsp-file-operations").default_capabilities()
+  --       ),
+  --     })
+  --   end,
+  -- },
   -- {
   --   "rachartier/tiny-inline-diagnostic.nvim",
   --   priority = 1000, -- needs to be loaded in first
@@ -169,8 +188,12 @@ return {
   --   event = "LspAttach",
   --   opts = { vt_position = "end_of_line" },
   -- },
-  { "Sebastian-Nielsen/better-type-hover", event = "LspAttach", opts = {} },
+  -- {
+  --   "Sebastian-Nielsen/better-type-hover",
+  --   ft = { "typescript", "typescriptreact" },
+  --   opts = {},
+  -- },
   -- get a hierarchical tree of references with :FunctionReferences (only if lsp feature is supported)
-  { "lafarr/hierarchy.nvim", event = "LspAttach", opts = {} },
+  -- { "lafarr/hierarchy.nvim", event = "LspAttach", opts = {} },
   -- { "m-demare/hlargs.nvim", opts = {} },
 }
