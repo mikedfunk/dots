48822129dd4af899eeab3f7add8782475a2c0534
Mike Funk <mike@mikefunk.com>
2026-02-04 20:20:55 -0800

    working

--- .config/swiftbar/ssh-tunnel-status.1s.sh
+++ .config/swiftbar/ssh-tunnel-status.1s.sh
@@ -7,7 +7,6 @@
 # SSH tunnel configuration
 SSH_HOST="saatchi-tunnel-aws-ssm"
 CONTROL_SOCKET="$HOME/.ssh/saatchi-tunnel-aws-ssm.sock"
-SSH_CONFIG="$HOME/.ssh/config"
 
 # Check tunnel status using SSH control socket
 if ssh -S "$CONTROL_SOCKET" "$SSH_HOST" -O check 2>/dev/null; then
@@ -17,18 +16,19 @@ else
 fi
 
 case "$status" in
-    connected)
-        echo "󰌘 | color=green"
-        echo "---"
-        echo "Tunnel is active"
-        echo "Disconnect | bash='/usr/bin/ssh' param1='-S' param2='$CONTROL_SOCKET' param3='$SSH_HOST' param4='-O' param5='exit' terminal=false refresh=true"
-        ;;
-    disconnected)
-        echo "󰌙 | color=red"
-        echo "---"
-        echo "Tunnel is inactive"
-        # Build connect command: source shell config, AWS SSO check, start privoxy, create tunnel
-        # Uses login shell to get full user environment (AWS config, SSH config, etc.)
-        echo "Connect | bash='/bin/zsh' param1='-ilc' param2='export PATH=\"/opt/homebrew/bin:/usr/local/bin:\$PATH\"; aws sts get-caller-identity >/dev/null 2>&1 || aws sso login; if ! [ \$(brew services info privoxy --json 2>/dev/null | jq -r \".[0].running\") = \"true\" ]; then brew services start privoxy; fi; ssh -f -M -S \"$CONTROL_SOCKET\" -F \"$SSH_CONFIG\" \"$SSH_HOST\"' terminal=true refresh=true"
-        ;;
+connected)
+    echo ":icloud.and.arrow.up: | sfcolor=green"
+    echo "---"
+    echo "Tunnel is active"
+    echo "Disconnect | bash='/usr/bin/ssh' param1='-S' param2='$CONTROL_SOCKET' param3='$SSH_HOST' param4='-O' param5='exit' terminal=false refresh=true"
+    ;;
+disconnected)
+    echo ":icloud.slash: | sfcolor=red"
+    echo "---"
+    echo "Tunnel is inactive"
+    # doesn't work
+    # Build connect command: source shell config, AWS SSO check, start privoxy, create tunnel
+    # Uses login shell to get full user environment (AWS config, SSH config, etc.)
+    # echo "Connect | $HOME/.support/saatchi/start-tunnel.sh terminal=true refresh=true"
+    ;;
 esac
